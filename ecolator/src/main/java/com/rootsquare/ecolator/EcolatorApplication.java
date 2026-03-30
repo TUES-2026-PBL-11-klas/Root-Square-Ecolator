7f52918b584b57ef0acba46dc9f280148cecfad1
Kristiyan Kobarelov <123277206+KristiyanKobarelov@users.noreply.github.com>
2026-03-30 17:51:46 +0300

    spring boot workspace

---
+++ ecolator/src/main/java/com/rootsquare/ecolator/EcolatorApplication.java
@@ -0,0 +1,13 @@
+package com.rootsquare.ecolator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolatorApplication.class, args);
+	}
+
+}
---
+++ ecolator/src/test/java/com/rootsquare/ecolator/EcolocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootsquare.ecolator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
